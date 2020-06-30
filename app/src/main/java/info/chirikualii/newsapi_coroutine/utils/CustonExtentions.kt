c1d6e9483ece41fd1a3a7568a942472450e59300
ChirikuAlii <chiriku.ali.rhmat@gmail.com>
2020-06-30 15:27:48 +0700

    remove unused class

--- app/src/main/java/info/chirikualii/newsapi_coroutine/utils/CustonExtentions.kt
+++ app/src/main/java/info/chirikualii/newsapi_coroutine/utils/CustonExtentions.kt
@@ -26,27 +26,5 @@ fun Long.timeMillisToDateTime(format: String?=null):String{
     return this.toString()
 }
 
-fun <T : Any?> Result<T?>.doSuccessOrFail(
-    success: (Result.Success<T?>) -> Unit,
-    fail: (Throwable) -> Unit
-) {
-    when (this) {
-        is Result.Success -> {
-            success.invoke(this)
-        }
-        is Result.Error -> {
-            fail.invoke(this.throwable)
-        }
-    }
-}
-
-suspend fun <T : Any> stateCall(call: suspend () -> Response<T>): Result<T> {
-    return if (call.invoke().isSuccessful) {
-        Result.Success(call.invoke().body()!!)
-    } else {
-        Result.Error(Throwable(call.invoke().message()))
-    }
-}
-
 
 
